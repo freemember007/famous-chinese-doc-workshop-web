43f44b7259a3b4c8a1c1590e93223c88b2d219f3
xjp <331000783@qq.com>
2020-04-10 16:51:10 +0800

    first deploy

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# --------------------
+echo '>>> 定义变量'
+# --------------------
+APP_PORT=4005
+APP_NAME=ddyy-common-business-react
+
+
+# --------------------
+echo '>>> 部署应用'
+# --------------------
+NODE_ENV=dev yarn
+
+pm2 ls | grep $APP_NAME
+
+if [ $? -eq 0 ]; then
+    yarn build
+    pm2 reload $APP_NAME
+else
+    yarn build
+    NODE_ENV=production pm2 start --name $APP_NAME  --log-date-format 'YYYY-MM-DD HH:mm:ss' ./node_modules/.bin/next -- start -p $APP_PORT
+fi
+
+
+# --------------------
+echo '>>> 测试一下'
+# --------------------
+sleep 3 #待待服务启动或重启完成
+curl http://api.diandianyy.com/ddyy-common-business-react
